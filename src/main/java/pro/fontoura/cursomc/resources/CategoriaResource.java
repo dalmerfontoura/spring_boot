e705e0cf345349ac52a7048dcfd039c4690c25e3
Dalmer Fontoura <dalmerfontoura@gmail.com>
2018-10-25 12:42:38 -0200

    Primeira classe de Dominio - Categoria

--- src/main/java/pro/fontoura/cursomc/resources/CategoriaResource.java
+++ src/main/java/pro/fontoura/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package pro.fontoura.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import pro.fontoura.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando! ";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lst = new ArrayList<Categoria>();
+		lst.add(cat1);
+		lst.add(cat2);
+		
+		return lst;
 		
 	}
 }
