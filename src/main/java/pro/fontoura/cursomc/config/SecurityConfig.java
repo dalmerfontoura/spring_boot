9738e7f51321001e2ae0de6555100e74b919e572
Dalmer Fontoura <dalmerfontoura@gmail.com>
2018-10-31 16:42:25 -0200

    Implementando autorização

--- src/main/java/pro/fontoura/cursomc/config/SecurityConfig.java
+++ src/main/java/pro/fontoura/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import pro.fontoura.cursomc.security.JWTAuthenticationFilter;
+import pro.fontoura.cursomc.security.JWTAuthorizationFilter;
 import pro.fontoura.cursomc.security.JWTUtil;
 
 @Configuration
@@ -41,8 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/categorias/**", 
 			"/clientes/**", 
-			"/produtos/**",
-			"/pedidos/**"
+			"/produtos/**"
 	};
 
 			
@@ -57,7 +57,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
-		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil))
+		.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		// isso desativa a criação de sessão no Spring Security
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
--- src/main/java/pro/fontoura/cursomc/security/JWTUtil.java
+++ src/main/java/pro/fontoura/cursomc/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -21,5 +22,35 @@ public class JWTUtil {
 		return Jwts.builder().setSubject(username).setExpiration(new Date(System.currentTimeMillis() + expitation))
 				.signWith(SignatureAlgorithm.HS512, secretKey.getBytes()).compact();
 		
+	}
+
+	public boolean tokenValido(String token) {
+		Claims claims = getClaims(token);
+		if (claims != null ) {
+			String username = claims.getSubject();
+			Date expiration = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+			
+			if(username != null && expiration != null && now.before(expiration)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private Claims getClaims(String token) {
+		try {
+			return Jwts.parser().setSigningKey(secretKey.getBytes()).parseClaimsJws(token).getBody();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public String getUserName(String token) {
+		Claims claims = getClaims(token);
+		if (claims != null ) {
+			return claims.getSubject();
+		}
+		return null;
 	}
 }
