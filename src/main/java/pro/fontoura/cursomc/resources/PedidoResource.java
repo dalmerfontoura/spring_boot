3c35806f992364ce137e4f3f966badc9122306ae
Dalmer Fontoura <dalmerfontoura@gmail.com>
2018-10-31 18:50:23 -0200

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/pro/fontoura/cursomc/resources/PedidoResource.java
+++ src/main/java/pro/fontoura/cursomc/resources/PedidoResource.java
@@ -5,11 +5,14 @@ import java.net.URI;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import pro.fontoura.cursomc.domain.Pedido;
@@ -29,6 +32,17 @@ public class PedidoResource extends ResourceDefault {
 		
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<Page<Pedido>> findPage(
+			@RequestParam(name = "page", defaultValue = "0") int page,
+			@RequestParam(name = "linesPerPages", defaultValue = "24") int linesPerPages,
+			@RequestParam(name = "orderBy", defaultValue = "instante") String orderBy,
+			@RequestParam(name = "direction", defaultValue = "DESC") String direction) {
+
+		Page<Pedido> pedidos = service.findToPages(page, linesPerPages, orderBy, direction);
+		return ResponseEntity.ok().body(pedidos);
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
--- src/main/java/pro/fontoura/cursomc/services/PedidoService.java
+++ src/main/java/pro/fontoura/cursomc/services/PedidoService.java
@@ -1,12 +1,17 @@
 package pro.fontoura.cursomc.services;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import pro.fontoura.cursomc.domain.Cliente;
 import pro.fontoura.cursomc.domain.ItemPedido;
 import pro.fontoura.cursomc.domain.PagamentoComBoleto;
 import pro.fontoura.cursomc.domain.Pedido;
@@ -14,10 +19,12 @@ import pro.fontoura.cursomc.domain.enuns.EstadoPagamento;
 import pro.fontoura.cursomc.repositories.ItemPedidoRepository;
 import pro.fontoura.cursomc.repositories.PagamentoRepository;
 import pro.fontoura.cursomc.repositories.PedidoRepository;
+import pro.fontoura.cursomc.security.UserSpringSecurity;
+import pro.fontoura.cursomc.services.exceptions.AuthorizationException;
 import pro.fontoura.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
-public class PedidoService {
+public class PedidoService implements ServiceInterface<Pedido, Pedido> {
 
 	@Autowired
 	private PedidoRepository repository;
@@ -39,12 +46,35 @@ public class PedidoService {
 	
 	@Autowired
 	private EmailService emailService;
+	
+	public PedidoService() {
+	}
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repository.findById(id);
 
 		return obj.orElseThrow(() -> new ObjectNotFoundException( id, Pedido.class.getName()));
 
+	}
+	
+	@Override
+	public List<Pedido> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public Page<Pedido> findToPages(int page, int linesPerPages, String orderBy, String direction) {
+		
+		UserSpringSecurity userSS = UserService.authenticated();
+		if(userSS == null) {
+			throw new AuthorizationException("Acesso Negado!");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPages, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(userSS.getId());
+		
+		return repository.findByCliente(cliente, pageRequest);
 	}
 
 	@Transactional
@@ -70,5 +100,23 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	@Override
+	public Pedido update(Pedido obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public Pedido fromDTO(Pedido objDto) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
